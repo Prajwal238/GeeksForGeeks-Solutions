4c84537b478940a70aefcd7658b159bf3aa05ae7
Prajwal238 <54891323+Prajwal238@users.noreply.github.com>
2022-04-19 14:07:07 +0530

    Create count-number-of-hops.cpp

---
+++ easy/count-number-of-hops.cpp
@@ -0,0 +1,46 @@
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to count the number of ways in which frog can reach the top.
+    #define mod 1000000007
+    long long countWays(int n)
+   {
+       
+       // your code here
+       if(n==0||n==1){
+           return 1;
+       }
+       long long dp[n+1];
+       dp[0]=1,dp[1]=1; dp[2]=2;
+       for(int i=3; i<=n; i++){
+           dp[i]=(dp[i-1]+dp[i-2]+dp[i-3])%1000000007;
+       }
+       return dp[n];
+   }
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+    //taking testcases
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    //taking number of steps in stair
+		int n;
+		cin>>n;
+		Solution ob;
+		//calling function countWays()
+		cout << ob.countWays(n) << endl;
+	}
+    
+    return 0;
+    
+}
+  // } Driver Code Ends
