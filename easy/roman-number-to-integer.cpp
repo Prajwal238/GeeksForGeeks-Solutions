e4685a3941841cd5bcc33f18a688da68c18750c3
Prajwal238 <54891323+Prajwal238@users.noreply.github.com>
2022-04-19 14:31:57 +0530

    Create roman-number-to-integer.cpp

---
+++ easy/roman-number-to-integer.cpp
@@ -0,0 +1,51 @@
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    int romanToDecimal(string s) {
+        unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        };
+        
+        int ans = 0;
+        int x = 0;
+        
+        for(int i = s.length() - 1;i>=0;i--){
+            if(roman[s[i]] >= x){
+                
+                ans+=roman[s[i]];
+                x = roman[s[i]];
+            }
+            else{
+                ans-=roman[s[i]];
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.romanToDecimal(s) << endl;
+    }
+}  // } Driver Code Ends
