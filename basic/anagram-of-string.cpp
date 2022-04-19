d6c25a0736b8f7377a634135ee854c25e59cdbe5
Prajwal238 <54891323+Prajwal238@users.noreply.github.com>
2022-04-19 13:36:38 +0530

    Create anagram-of-string.cpp

---
+++ basic/anagram-of-string.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int CHARS = 26;
+
+// function to calculate minimum numbers of characters
+// to be removed to make two strings anagram
+int remAnagram(string str1, string str2);
+
+// Driver program to run the case
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str1,str2;
+        cin>>str1>>str2;
+        cout << remAnagram(str1, str2);
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// function to calculate minimum numbers of characters
+// to be removed to make two strings anagram
+int remAnagram(string str1, string str2)
+{
+// Your code goes here
+    int h1[26]={0},h2[26] = {0};
+    
+    for(int i=0;i<str1.length();i++){
+        h1[str1[i]-'a']++;
+    }
+    for(int i=0;i<str2.length();i++){
+        h2[str2[i]-'a']++;
+    }
+    int count=0;
+    
+    for(int i=0;i<26;i++){
+        if(h1[i]>h2[i])
+            count+=h1[i]-h2[i];
+        else if(h2[i]>h1[i])
+            count+=h2[i]-h1[i];
+    }
+    return count;
+}
