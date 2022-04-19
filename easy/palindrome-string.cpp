2cc948af70952b543319c5d3d121c02d8428bbe0
Prajwal238 <54891323+Prajwal238@users.noreply.github.com>
2022-04-19 14:14:05 +0530

    Create palindrome-string.cpp

---
+++ easy/palindrome-string.cpp
@@ -0,0 +1,47 @@
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    for(int i =0;i<S.length();i++){
+	        if(S[i] != S[S.length()-i - 1]){
+	           // cout<<S[i]<<S[S.length()-i - 1]<<"\n";
+	            return 0;
+	        }
+	    }
+	    return 1;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
